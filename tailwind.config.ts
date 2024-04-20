ffeaf9f27ffcf8e559ad95e30d53dd5c3fe8d7a0
Mohammad Ali <itsproali@gmail.com>
2024-04-21 00:47:16 +0600

    Initialized

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: "#60A534",
+        },
+        secondary: {
+          DEFAULT: "#D9FA06",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-kardia)", "sans-serif"],
+        kameron: ["var(--font-kameron)", "sans-serif"],
       },
     },
   },
